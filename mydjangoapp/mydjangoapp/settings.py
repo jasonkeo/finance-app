784c6e3687149191abddee9551ec56336df2e4fd
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2024-09-03 13:46:10 +0000

    f

--- mydjangoapp/mydjangoapp/settings.py
+++ mydjangoapp/mydjangoapp/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # or wherever your static files are
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
