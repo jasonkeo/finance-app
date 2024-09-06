0e841cf2cb998c6f3319b557a2123387a5ef087b
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2024-09-06 12:49:09 +0000

    fea

--- sync.sh
+++ sync.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 # Navigate to the app directory
-cd the_app/ || exit
+cd the_app/ || echo "Directory not found"
 
 # Pull the latest changes from Git
 echo "Pulling the latest changes"
-git pull origin main > /dev/null
+git reset --hard= > /dev/null
+git pull origin main --force
 
-# # Pause for 5 seconds (optional)
-# sleep 5
+# Pause for 5 seconds (optional)
+sleep 5
 
-# # Restart the server
-# echo "Restarting the server"
-# docker stop $(docker ps -q) > /dev/null
-# docker rm $(docker ps -a -q) > /dev/null
+# Restart the server
+echo "Restarting the server"
+docker stop $(docker ps -q) > /dev/null
+docker rm $(docker ps -a -q) > /dev/null
 
-# # Pause for 5 seconds (optional)
-# sleep 5
+# Pause for 5 seconds (optional)
+sleep 5
 
-# # Rebuild and bring up containers
-# docker-compose up --build --remove-orphans -d > /dev/null
-# echo "Server is up and running"
+# Rebuild and bring up containers
+docker-compose up --build --remove-orphans -d
+echo "Server is up and running"
 
-# # Navigate to the frontend directory and manually run npm dev
-# cd frontend/ || exit
-# PORT=3000 npm run dev
+# Navigate to the frontend directory and manually run npm dev
+cd frontend/ || echo "Directory not found"
 
-# # Show running containers
-# docker ps
+
+# Show running containers
+docker ps
