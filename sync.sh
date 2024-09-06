cc4d9a8f5c3b4d8706e9bf3a7713fd81fad9cf83
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2024-09-06 12:26:58 +0000

    dwa

--- sync.sh
+++ sync.sh
@@ -3,9 +3,11 @@ git pull origin main > /dev/null
 echo "Pulling the latest changes"
 sleep 5
 echo "Restarting the server"
-docker compose down > /dev/null
+docker stop $(docker ps -q) > /dev/null
+docker rm $(docker ps -a -q) > /dev/null
 sleep 5
-docker compose up --build --remove-orphans > /dev/null
+docker build -no-cache > /dev/null
+docker compose up --remove-orphans > /dev/null
 echo "Server is up and running"
 cd frontend/
 PORT=3000 npm run dev
