c81f35843cce525413aa61a3e9c8a3bc7e251dfc
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2024-09-11 11:25:36 +0000

    fea

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -11,20 +11,24 @@ def my_periodic_task():
     headers = {
         'Authorization': f'Bearer {api_key}'
     }
-    response = requests.get('https://newsapi.org/v2/top-headlines?country=us', headers=headers)
+    response = requests.get('https://newsapi.org/v2/top-headlines?country=us&totalresults=5', headers=headers)
     
     if response.status_code == 200:
         news = response.json()
         print("News fetched successfully!")
-        print(news)
         temp = []
         for i in news['articles']:
             temp.append(i['title'])
 
-        send = {
+        post_data = {
             'date': todays_date,
             'news': temp
         }
-        add = requests.post('http://127.0.0.1:8000/api/news/', json=send)
+        add = requests.post(
+            'http://127.0.0.1:8000/api/news/',
+            json=post_data,  # Ensure that the data is sent as JSON
+            headers={'Content-Type': 'application/json'}
+        )
+
     else:
         print(f"Failed to fetch news. Status code: {response.status_code}")
