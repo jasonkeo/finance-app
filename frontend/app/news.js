bf383bd89195e42f82c0c94d91e323a075756f29
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2024-10-02 05:56:35 +0000

    daw

--- frontend/app/news.js
+++ frontend/app/news.js
@@ -1,7 +1,13 @@
+import { ChevronLeft } from "lucide-react";
 export default function News() {
   return (
     <div className="p-5 bg-white min-w-[200px] rounded-lg shadow-md transition-transform transition-shadow hover:shadow-custom hover:translate-y-[-4.30px]">
-      News
+      <div className="flex justify-between">Today News 
+
+        <div>
+          <ChevronLeft />
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/app/stats.js
+++ frontend/app/stats.js
@@ -4,7 +4,13 @@ import { LineChart } from '@mui/x-charts/LineChart';
 export default function Stats() {
     return (
         <div className=" p-5 bg-white min-w-[500px] rounded-lg shadow-md transition-transform transition-shadow hover:shadow-custom hover:translate-y-[-4.30px]">
-            Stats
+            <div className='flex justify-between'> Stats 
+            <select className="ml-2 p-1 border rounded">
+                    <option value="option1">Option 1</option>
+                    <option value="option2">Option 2</option>
+                    <option value="option3">Option 3</option>
+                </select>
+            </div>
             <LineChart
                 xAxis={[{ data: [1, 2, 3, 5, 8, 10] }]}
                 series={[
